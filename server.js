9ee5cb2dae61d6ff1fbdafd3bf5e0977d9921d9c
Thiago Giammattey <giammattey@gmail.com>
2020-01-28 13:43:14 -0300

    adiciona dados dinâmicos

--- server.js
+++ server.js
@@ -2,21 +2,45 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-	express: server
+	express: server,
+	autoescape: false
 })
 
 server.get('/', (req, res) => {
-	return res.render('about')
+	const about = {
+		avatar_url: 'https://avatars2.githubusercontent.com/u/6643122?s=400&v=4',
+		name: 'Mayk Brito',
+		role: 'Instrutor - Rocketseat',
+		description:
+			'Programador fullstack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br/" target="_blank">Rocketseat</a>.',
+		links: [
+			{
+				name: 'Github',
+				url: 'https://github.com/maykbrito/'
+			},
+			{
+				name: 'Twitter',
+				url: 'https://twitter.com/maykbrito/'
+			},
+			{
+				name: 'Linkedin',
+				url: 'https://www.linkedin.com/in/maykbrito/'
+			}
+		]
+	}
+
+	return res.render('about', { about })
 })
 
 server.get('/portfolio', (req, res) => {
-	return res.render('portfolio')
+	return res.render('portfolio', { items: videos })
 })
 
 server.listen(5000, function() {
